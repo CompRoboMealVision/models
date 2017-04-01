51a619a197ac56af850239512b5e5410ea4dc539
David Zhu <hdavidzhu@gmail.com>
2017-03-31 22:07:46 -0400

    Copied over flower data training scripts to foods data.

---
+++ inception/inception/foods_eval.py
@@ -0,0 +1,23 @@
+"""A binary to evaluate Inception on the foods data set.
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+from inception import inception_eval
+from inception.foods_data import FoodsData
+
+FLAGS = tf.app.flags.FLAGS
+
+def main(unused_argv=None):
+  dataset = FoodsData(subset=FLAGS.subset)
+  assert dataset.data_files()
+  if tf.gfile.Exists(FLAGS.eval_dir):
+    tf.gfile.DeleteRecursively(FLAGS.eval_dir)
+  tf.gfile.MakeDirs(FLAGS.eval_dir)
+  inception_eval.evaluate(dataset)
+
+if __name__ == '__main__':
+  tf.app.run()
